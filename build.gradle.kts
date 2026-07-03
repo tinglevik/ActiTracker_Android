4d8659f30d847874c298514ea8b6dcf43a4c3895
Anonymous <anonymous@users.noreply.github.com>
2026-07-03 03:55:02 +0300

    build: update Kotlin Compose plugin to 2.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,5 @@
 plugins {
     id("com.android.application") version "9.2.1" apply false
     id("com.google.devtools.ksp") version "2.3.2" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
 }
